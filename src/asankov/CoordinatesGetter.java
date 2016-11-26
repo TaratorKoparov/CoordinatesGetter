fc265dca5781a01bd42eefbe9319df9fe03687b6
Planet team 1 - Team Rocket <hslavov91@gmail.com>
2016-11-26 14:30:11 +0200

    Update CoordinatesGetter.java

--- src/asankov/CoordinatesGetter.java
+++ src/asankov/CoordinatesGetter.java
@@ -11,11 +11,7 @@ import java.nio.file.Paths;
 public class CoordinatesGetter {
 
 	public static void main(String[] args) {
-		CoordinatesArray car = new CoordinatesArray(10.0, 15.0, 20.0, 25.0);
-		
-		String json = car.getGEOJSON();
-		
-		System.out.println(json);
+		CoordinatesArray car = new CoordinatesArray(10.0, 15.0, 20.0, 25.0); 
 		
 		String fileName = "geoJSON.json";
 		Path filePath = Paths.get(fileName);
